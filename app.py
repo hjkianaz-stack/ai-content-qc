5a5964017734e0a3e47818fdf647435a6adffdc5
hjkianaz-stack <hjkianaz@gmail.com>
2026-09-21 13:49:32 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1232,4 +1232,9 @@ with gr.Blocks(
 # LAUNCH
 # =========================================================
 
-app.launch()
+import os
+
+app.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 10000))
+)
